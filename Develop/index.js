abac84524e8ce03cba2fdc604440c7a436edf8a6
musaliyah <asmusaliar@gmail.com>
2022-08-03 12:29:23 -0700

    questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,66 @@
-// TODO: Include packages needed for this application
+//packages 
+
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+const generateMarkdown = require ('generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //Title
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? '
+    },
+    //Description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project: '
+    },
+    //Installation
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps to install your project?'
+    },
+    // Usage
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions of how to use your project: '
+    },
+    // Credits
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators GitHub usernames'
+    },
+    // Testing
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'What tests can you perform?'
+    },
+    // License (License list from: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba)
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select what license type your project is: ',
+        choices:['Apache', 'Boost', 'BSD', 'Creative Commons', 'Eclipse', 'GNU', 'IBM', 'ISC', 'MIT', 'Mozilla', 'Open Database', 'Public Domain Dedication and License', 'Perl', 'SIL', 'Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your github username: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email: '
+    }
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
